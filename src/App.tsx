0836f972eb07c0e50020bea071a57d96a456ece3
Ujwal Rajeev <ujwalrajeev@gmail.com>
2026-03-30 17:53:11 +0100

    Add home page container and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import "./App.scss";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <div className="main-container">
+      <div className="first-look-container"></div>
+    </div>
+  );
+}
